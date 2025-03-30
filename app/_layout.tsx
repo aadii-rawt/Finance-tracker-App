7027075a12efa7a0b4b672a9a1dae0826a987391
adityarawat4 <rawatadii060@gmail.com>
2025-03-30 20:03:57 +0530

    header title

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,25 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { AuthProvider } from '../context/AuthContext';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { AuthProvider } from "../context/AuthContext";
 import ToastNotification from "../components/ToastNotification";
+import { TouchableOpacity } from "react-native";
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -29,18 +34,29 @@ export default function RootLayout() {
 
   return (
     <AuthProvider>
-    <ThemeProvider value={DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      {/* <Stack.Screen name="index" options={{ headerShown: false }} /> */}
-      <Stack.Screen name="login"/>
+      <ThemeProvider value={DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          {/* <Stack.Screen name="index" options={{ headerShown: false }} /> */}
+          <Stack.Screen name="login" />
+          <Stack.Screen
+            name="addIncomeExpense"
+            options={({ navigation }) => ({
+              title: "Add Income and Expense",
+              headerLeft: () => (
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                  {/* <Ionicons name="arrow-back" size={24} color="black" /> */} +
+                </TouchableOpacity>
+              ),
+            })}
+          />
 
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
 
-      <ToastNotification />
-    </ThemeProvider>
+        <ToastNotification />
+      </ThemeProvider>
     </AuthProvider>
   );
 }
