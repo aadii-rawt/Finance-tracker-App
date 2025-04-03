304c6fe43e3e3289c928f21ed5ed425d7945cd18
adityarawat4 <rawatadii060@gmail.com>
2025-04-03 17:07:21 +0530

    more screen ui

--- app/(tabs)/more.jsx
+++ app/(tabs)/more.jsx
@@ -42,14 +42,14 @@ export default function More() {
 
       {/* Grid Menu */}
       <View style={styles.grid}>
-        <GridItem icon={<MaterialIcons name="person-outline" size={24} color="#26897C" />} label="Personal Profile" />
-        <GridItem icon={<FontAwesome name="gear" size={24} color="#26897C" />} label="Invite Friends" />
-        <GridItem icon={<FontAwesome name="bank" size={24} color="#26897C" />} label="Invite Friends" />
-        {/* <GridItem icon={<Ionicons name="person" size={24} color="#26897C" />} label="Account Info" /> */}
-        <GridItem icon={<Ionicons name="mail-outline" size={24} color="#26897C" />} label="Message Center" />
-        <GridItem icon={<Ionicons name="lock-closed-outline" size={24} color="#26897C" />} label="Login & Security" />
-        {/* <GridItem icon={<Entypo name="lock" size={24} color="#26897C" />} label="Data & Privacy" /> */}
-        <GridItem icon={<MaterialIcons name="logout" size={24} color="red" />} label="Logout" onPress={handleLogout} />
+        <GridItem icon={<MaterialIcons name="person-outline" size={28} color="#26897C" />} label="Profile" />
+        <GridItem icon={<FontAwesome name="gear" size={28} color="#26897C" />} label="Configuration" />
+        <GridItem icon={<FontAwesome name="bank" size={28} color="#26897C" />} label="Account" />
+        {/* <GridItem icon={<Ionicons name="person" size={28} color="#26897C" />} label="Account Info" /> */}
+        <GridItem icon={<Ionicons name="mail-outline" size={28} color="#26897C" />} label="Feedback" />
+        <GridItem icon={<Ionicons name="lock-closed-outline" size={28} color="#26897C" />} label="Login & Security" />
+        {/* <GridItem icon={<Entypo name="lock" size={28} color="#26897C" />} label="Data & Privacy" /> */}
+        <GridItem icon={<MaterialIcons name="logout" size={28} color="red" />} label="Logout" onPress={handleLogout} />
       </View>
     </SafeAreaView>
   );
@@ -64,8 +64,8 @@ const GridItem = ({ icon, label, onPress }) => (
 
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: '#fff', alignItems : "center", justifyContent : "center" },
-  avatarSection: { alignItems: 'center', marginVertical: 50 },
-  avatar: { width: 100, height: 100, borderRadius: 50, borderWidth: 3, borderColor: '#26897C' },
+  avatarSection: { alignItems: 'center', marginVertical: 50 ,width: 100, height: 100, borderRadius: 50, borderRadius: 50, borderWidth: 3, borderColor: '#26897C' },
+  avatar: { width: "100%", height: "100%", },
   name: { fontSize: 18, fontWeight: '700', marginTop: 10 },
   grid: {
     flexDirection: 'row',
@@ -87,7 +87,7 @@ const styles = StyleSheet.create({
   gridLabel: {
     marginTop: 8,
     textAlign: 'center',
-    fontSize: 12,
+    fontSize: 15,
     fontWeight: '500',
   },
 });
